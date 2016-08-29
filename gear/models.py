172df810d24e59409e60cae00b9707392df59366
J. A. Mercury <jeremiah.a.mercury@gmail.com>
2016-08-28 23:00:23 -0400

    added equippable flag

--- gear/models.py
+++ gear/models.py
@@ -14,6 +14,7 @@ class Item(models.Model):
 	])
 	level = models.IntegerField(null = True, blank = True)
 	description = models.CharField(max_length = 200, unique = True, null = True)
+	equippable = models.BooleanField(default = False)
 
 	def __str__(self):
 		return self.name
