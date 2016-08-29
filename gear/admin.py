70d76618825afed1b7c036575333115d0777dea1
J. A. Mercury <jeremiah.a.mercury@gmail.com>
2016-08-28 21:14:47 -0400

    lots of didn't works in here; plus ignoring DB from now on

--- gear/admin.py
+++ gear/admin.py
@@ -8,16 +8,15 @@ from .models import Recipe, Item, Ingredient
 
 class IngredientInlineForm(forms.ModelForm):
 	""" Def class"""
-	
+
 	class Meta:
 		model = Ingredient
-		fields = '__all__' 
+		fields = '__all__'
 
 	def __init__(self, *args, **kwargs):
 		super(IngredientInlineForm, self).__init__(*args, **kwargs)
 		try:
 			self.fields['item'].queryset = Item.objects.exclude(item_name=self.instance.recipe.item.item_name)
-			pass
 		except ObjectDoesNotExist:
 			pass
 
@@ -26,13 +25,16 @@ class IngredientInline(nested_admin.NestedTabularInline):
 	""" Def class"""
 	model = Ingredient
 	form = IngredientInlineForm
-	extra = 1
+	extra = 0
 
 
 class RecipeInline(nested_admin.NestedTabularInline):
 	""" Def class"""
 	model = Recipe
 	inlines = [IngredientInline]
+	max_num = 1
+	extra = 0
+	validate_max = True
 
 
 class ItemAdmin(nested_admin.NestedModelAdmin):
--- gear/models.py
+++ gear/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Item(models.Model):
 	"""The smallest denominator. Could be scraps, scrolls, or equippable items"""
-	item_name = models.CharField(max_length = 200, unique = True, default= "item name")
-	item_color = models.IntegerField(choices = [(1, 'white'), (2, 'green')], default = 1)
-	
+	item_name = models.CharField(max_length = 200, unique = True)
+	item_color = models.IntegerField(choices = [(1, 'white'), (2, 'green')])
+
 	def __str__(self):
 		return self.item_name
 
@@ -13,16 +13,16 @@ class Item(models.Model):
 class Recipe(models.Model):
 	"""Add Recipe def"""
 	item = models.OneToOneField(Item, on_delete = models.CASCADE)
-	
+
 	def __str__(self):
 		return self.item.item_name + " recipe"
-		
+
 
 class Ingredient(models.Model):
 	"""Add Ingredient def"""
 	recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
 	item = models.ForeignKey(Item, on_delete = models.CASCADE)
-	quantity = models.IntegerField(default=0)
+	quantity = models.IntegerField()
 
 	def __str__(self):
 		return str(self.quantity) + " " + self.item.item_name
