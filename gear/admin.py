094cfaffd12465c3efe6c2e6376959e1bc8b39e4
J. A. Mercury <jeremiah.a.mercury@gmail.com>
2016-09-02 22:50:52 -0400

    rearranging again

--- gear/admin.py
+++ gear/admin.py
@@ -49,7 +49,7 @@ class ItemAdmin(nested_admin.NestedModelAdmin):
 		(None, {'fields':['name', 'color', 'level', 'description', 'equippable']}),
 	]
 	inlines = [StatInline, RecipeInline]
-	list_display = ('equippable', 'name', 'color', 'level', 'description')
+	list_display = ('name', 'color', 'level', 'description', 'equippable')
 	list_filter = ['color', 'equippable', 'level']
 	search_fields = ['name', 'description']
 
