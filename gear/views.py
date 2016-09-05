1a94598ee4643d9173e08626d6ac36afae539da7
J. A. Mercury <jeremiah.a.mercury@gmail.com>
2016-09-05 02:14:08 -0400

    trying a different limiter

--- gear/views.py
+++ gear/views.py
@@ -14,20 +14,22 @@ def processItems(item, items, multiplier):
 		pass
 
 def index(request):
-	ids = [6, 5, 4, 3]
-	heroes = Hero.objects.all().exclude(id__in = ids)
+	heroes = Hero.objects.all()
 	items = {}
 	for hero in heroes:
 		hero.rarities = hero.rarity_set.all()
+		i = 1;
 		for rarity in hero.rarities:
-			if None == rarity.level:
-					level = ''
-			else:
-					level = '+' + str(rarity.level)
-			label = hero.name + rarity.get_color_display() + ' ' + level
-			for i in range(1,7):
-				items[label + '_' + str(i)] = {}
-				processItems(getattr(rarity, 'gear' + str(i)), items[label + '_' + str(i)], 1)
+			if i < 5:
+				if None == rarity.level:
+						level = ''
+				else:
+						level = '+' + str(rarity.level)
+				label = hero.name + rarity.get_color_display() + ' ' + level
+				for i in range(1,7):
+					items[label + '_' + str(i)] = {}
+					processItems(getattr(rarity, 'gear' + str(i)), items[label + '_' + str(i)], 1)
+			i += 1
 	context = {
 		'heroes' : heroes,
 		'items' : items,
