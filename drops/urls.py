5e93ef4ecbd2c25aa6958e1a87cb5b86f4882bd5
J. A. Mercury <jeremiah.a.mercury@gmail.com>
2016-10-01 00:09:36 -0400

    added in memory states, and full calculation heading into the results for grind optimizer

--- drops/urls.py
+++ drops/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^results/$', views.index, name='index'),
 	url(r'^results/(?P<ingredients_raw>[0-9_:]+)$', views.results, name='results'),
 	url(r'^.*', views.index, name='index'),
 ]
