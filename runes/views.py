39079d805ab5426b53d46a58fb63e86b66b26991
J. A. Mercury <jeremiah.a.mercury@gmail.com>
2016-11-17 16:18:06 -0500

    first pass at runes front end

--- runes/views.py
+++ runes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from heroes.models import Hero
+from stats.models import Stat
+from .models import Collection
+import copy
 
-# Create your views here.
+def index(request):
+    heroes = Hero.objects.all()
+    stats = Stat.objects.all()
+    collections = Collection.objects.all()
+    context = {
+        'heroes' : heroes,
+        'stats' : stats,
+        'collections' : collections,
+    }
+    return render(request, 'runes/index.html', context)
