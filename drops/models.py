6d10a42702e0f555c22d05f57ccdc5c8a96ded99
J. A. Mercury <jeremiah.a.mercury@gmail.com>
2016-11-20 21:33:56 -0500

    added max_chapter support

--- drops/models.py
+++ drops/models.py
@@ -3,35 +3,33 @@ from gear.models import Item, Recipe, Ingredient
 
 
 class Chapter(models.Model):
-	"""A group of stages = chapter, in the campaign """
-	number = models.IntegerField(null = True, blank = False)
-	name = models.CharField(max_length = 200, unique = True, null = True)
-	minlevel = models.IntegerField(null = True)
-	cost = models.IntegerField(null = True)
-	
-	class Meta:
-		ordering = ['number']
+    """A group of stages = chapter, in the campaign """
+    number = models.IntegerField(null = True, blank = False)
+    name = models.CharField(max_length = 200, unique = True, null = True)
+    minlevel = models.IntegerField(null = True)
+    cost = models.IntegerField(null = True)
 
-	def __str__(self):
-		#fullname = str(self.level) + ':' + self.name
-		return str(self.number) + ': ' + str(self.name)
+    class Meta:
+        ordering = ['number']
+
+    def __str__(self):
+        return str(self.number) + ': ' + str(self.name)
 
 
 class Stage(models.Model):
-	#A stage in the campaign
-	chapter = models.ForeignKey(Chapter, null = True, on_delete = models.CASCADE, related_name='chapter')
-	number = models.IntegerField(null = True, blank = False)
-	name = models.CharField(max_length = 200, unique = True, null = True)
+    #A stage in the campaign
+    chapter = models.ForeignKey(Chapter, null = True, on_delete = models.CASCADE, related_name='chapter')
+    number = models.IntegerField(null = True, blank = False)
+    name = models.CharField(max_length = 200, unique = True, null = True)
 
-	def __str__(self):
-		#fullname = str(self.level) + ':' + self.name
-		return str(self.chapter.number) + '-' + str(self.number) + ': ' + self.name
+    def __str__(self):
+        return str(self.chapter.number) + '-' + str(self.number) + ': ' + self.name
 
 
 class Drop(models.Model):
-	#Gear dropped in a stage
-	stage = models.ForeignKey(Stage, on_delete = models.CASCADE, related_name='drops')
-	item = models.ForeignKey(Item, limit_choices_to={'recipe': None}, on_delete = models.CASCADE, null = True, related_name='drops')
+    #Gear dropped in a stage
+    stage = models.ForeignKey(Stage, on_delete = models.CASCADE, related_name='drops')
+    item = models.ForeignKey(Item, limit_choices_to={'recipe': None}, on_delete = models.CASCADE, null = True, related_name='drops')
 
-	def __str__(self):
-		return self.stage.name + ' drops ' + self.item.name
+    def __str__(self):
+        return self.stage.name + ' drops ' + self.item.name
--- drops/views.py
+++ drops/views.py
@@ -1,56 +1,59 @@
 from django.shortcuts import render
 from gear.models import Item
 from heroes.models import Hero, Rarity
-from .models import Drop
+from .models import Drop, Chapter
 import copy
 
-def index(request, ingredients_raw = ''):
-	heroes = Hero.objects.all().prefetch_related(
-		'rarities__gear1',
-		'rarities__gear2',
-		'rarities__gear3',
-		'rarities__gear4',
-		'rarities__gear5',
-		'rarities__gear6',
-		'quests'
-	)
-	next_step = ''
-	ingredients = ''
-	drops = ''
-	if('' != ingredients_raw):
-		sets = ingredients_raw.split('_')
-		ingredients = []
-		for pair in sets:
-			values = pair.split(':');
-			ingredients.append({ 'item' : int(values[0]), 'quantity' : int(values[1]) });
-		ingredients = sorted(ingredients, key=lambda k: k['quantity'], reverse=True)
-		drops = Drop.objects.filter(item = ingredients[0]['item']).order_by('-id')
-		if 1 != len(drops):
-			for ingredient in ingredients[1:]:
-				drops_new = []
-				for drop in drops:
-					for item in drop.stage.drops.all():
-						if item.item.id == ingredient['item']:
-							drops_new.append(drop)
-							break
-				if 0 < len(drops_new):
-					drops = copy.deepcopy(drops_new)
-					if 1 == len(drops_new):
-						break
-		stage = drops[0].stage
-		scraps = []
-		drop_items = {}
-		for drop in stage.drops.all():
-			drop_items[drop.item.id] = drop.item
-		for ingredient in ingredients:
-			if ingredient['item'] in drop_items:
-				scraps.append({'item' : drop_items[ingredient['item']], 'quantity' : ingredient['quantity']})
-		next_step = {
-			'stage' : stage,
-			'scraps' : scraps
-		}
-	context = {
-		'heroes' : heroes,
-		'next' : next_step,
-	}
-	return render(request, 'drops/index.html', context)
+def index(request, max_chapter = 0, ingredients_raw = ''):
+    chapters = Chapter.objects.all().order_by('-number')
+    heroes = Hero.objects.all().prefetch_related(
+        'rarities__gear1',
+        'rarities__gear2',
+        'rarities__gear3',
+        'rarities__gear4',
+        'rarities__gear5',
+        'rarities__gear6',
+        'quests'
+    )
+    next_step = ''
+    ingredients = ''
+    drops = ''
+    if('' != ingredients_raw):
+        sets = ingredients_raw.split('_')
+        ingredients = []
+        for pair in sets:
+            values = pair.split(':');
+            ingredients.append({ 'item' : int(values[0]), 'quantity' : int(values[1]) });
+        ingredients = sorted(ingredients, key=lambda k: k['quantity'], reverse=True)
+        drops = Drop.objects.filter(item = ingredients[0]['item']).filter(stage__chapter__id__lt = max_chapter).order_by('-id')
+        if 1 != len(drops):
+            for ingredient in ingredients[1:]:
+                drops_new = []
+                for drop in drops:
+                    for item in drop.stage.drops.all():
+                        #if item.stage.chapter.id <= max_chapter and item.item.id == ingredient['item']:
+                        if item.item.id == ingredient['item']:
+                            drops_new.append(drop)
+                            break
+                if 0 < len(drops_new):
+                    drops = copy.deepcopy(drops_new)
+                    if 1 == len(drops_new):
+                        break
+        stage = drops[0].stage
+        scraps = []
+        drop_items = {}
+        for drop in stage.drops.all():
+            drop_items[drop.item.id] = drop.item
+        for ingredient in ingredients:
+            if ingredient['item'] in drop_items:
+                scraps.append({'item' : drop_items[ingredient['item']], 'quantity' : ingredient['quantity']})
+        next_step = {
+            'stage' : stage,
+            'scraps' : scraps
+        }
+    context = {
+        'heroes' : heroes,
+        'next' : next_step,
+        'chapters' : chapters,
+    }
+    return render(request, 'drops/index.html', context)
