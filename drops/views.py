e2dc151d57c4552287a21b8ad8cb6e70613f0e91
Dana Mercury <danaamercury@gmail.com>
2016-11-27 17:52:38 -0500

    more debugging

--- drops/views.py
+++ drops/views.py
@@ -105,6 +105,8 @@ def index(request, max_chapter = 0, ingredients_raw = '', candidates_raw = ''):
 		needed = items[winner['unique_id']]
 		needed_ids = list(needed.keys())
 		stages = []
+		debug1 = []
+		debug2 = []
 		for item_id in needed_ids:
 			if item_id not in covered and ingredients[item_id]['quantity'] < needed[item_id]['total']:
 				drops = Drop.objects.filter(item = item_id).filter(stage__chapter__id__lte = max_chapter).order_by('-id').prefetch_related('stage__drops')
@@ -125,6 +127,7 @@ def index(request, max_chapter = 0, ingredients_raw = '', candidates_raw = ''):
 					if drops[0].stage.id not in stages:
 						stages.append(stage.id)
 					for drop in stage.drops.all():
+						debug1.append(drop.item.id)
 						if drop.item.id in needed_ids and drop.item.id not in covered:
 							covered.append(drop.item.id)
 				else:
@@ -136,6 +139,7 @@ def index(request, max_chapter = 0, ingredients_raw = '', candidates_raw = ''):
 				scraps = []
 				stage = Stage.objects.get(id=stage)
 				for drop in stage.drops.all():
+					debug2.append(drop.item.id)
 					if drop.item.id in needed_ids and drop.item.id not in covered:
 						covered.append(drop.item.id)
 						quantity = needed[drop.item.id]['total'] - ingredients[drop.item.id].quantity
@@ -151,5 +155,7 @@ def index(request, max_chapter = 0, ingredients_raw = '', candidates_raw = ''):
 		'failed' : failed,
 		'covered' : covered,
 		'needed_ids' : needed_ids,
+		'debug1' : debug1,
+		'debug2' : debug2
 	}
 	return render(request, 'drops/index.html', context)
