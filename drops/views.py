19d45c9126d3f7e43f9b9a53dbcb5f71c301b860
Dana Mercury <danaamercury@gmail.com>
2016-11-27 17:30:41 -0500

    trying to fix queryset issue

--- drops/views.py
+++ drops/views.py
@@ -135,7 +135,7 @@ def index(request, max_chapter = 0, ingredients_raw = '', candidates_raw = ''):
 			for stage in stages:
 				scraps = []
 				stage = Stage.objects.filter(id=stage).prefetch_related('drops')
-				for drop in stage.drops.all():
+				for drop in stage.drops:
 					if drop.item.id in needed_ids and drop.item.id not in covered:
 						covered.append(drop.item.id)
 						quantity = needed[drop.item.id]['total'] - ingredients[drop.item.id].quantity
